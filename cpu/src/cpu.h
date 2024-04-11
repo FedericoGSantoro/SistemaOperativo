afe9ceb08a40fcbe6abbe90d4486ebe877b87da7
ailenolha13 <aolha@frba.utn.edu.ar>
2024-04-11 23:42:45 +0000

    funcion leerConfig modulo cpu

--- cpu/src/cpu.h
+++ cpu/src/cpu.h
@@ -4,11 +4,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "../../utils/src/sockets/sockets.h"
+#include <commons/log.h>
+#include <commons/config.h>
+#include "../../utils/src/config/configs.h"
 
+#define rutaConfiguracionCpu "/home/utnso/Desktop/TP-SO/tp-2024-1c-Grupo-AFFOM/cpu/cpu.config"
 
+t_log* cpu_logger;
+t_config* configuracion;
 
+char* IP_MEMORIA;
+int PUERTO_MEMORIA;
+int PUERTO_ESCUCHA_DISPATCH;
+int PUERTO_ESCUCHA_INTERRUPT;
+int CANTIDAD_ENTRADAS_TLB;
+char* ALGORITMO_TLB;
 
-
-
+void leerConfig();
 
 #endif
