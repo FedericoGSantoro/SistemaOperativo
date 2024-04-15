8297503240a82c86f26b492d99b7f5e00f0586e7
utnso <fwaldman@frba.utn.edu.ar>
2024-04-15 15:10:29 +0000

    Avances C1 I/O

--- entradasalida/src/entradasalida.c
+++ entradasalida/src/entradasalida.c
@@ -1,8 +1,83 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "../../utils/src/sockets/sockets.h"
+#include "entradasalida.h"
 
 int main(int argc, char* argv[]) {
-    decir_hola("una Interfaz de Entrada/Salida");
+    
+    inicializar();
+    system("sleep 5");
+
+    terminarPrograma();
+
     return 0;
+}
+
+void inicializar(){
+    inicializarLogs();
+
+    inicializarConfig();
+
+    inicializarConexiones();
+}
+
+void inicializarLogs(){
+    logger_obligatorio = log_create("entradasalida.log", "LOG_OBLIGATORIO_ENTRADA-SALIDA", true, LOG_LEVEL_INFO);
+    logger_auxiliar = log_create("entradasalidaExtras.log", "LOG_EXTRA_ENTRADA_SALIDA", true, LOG_LEVEL_INFO);
+    logger_error = log_create("entradasalidaExtras.log", "LOG_ERROR_ENTRADA_SALIDA", true, LOG_LEVEL_ERROR);
+    // Compruebo que los logs se hayan creado correctamente
+    if (logger_auxiliar == NULL || logger_obligatorio == NULL || logger_error == NULL) {
+        terminarPrograma();
+        abort();
+    }
+}
+
+void inicializarConfig(){
+    configuracion = iniciar_config(rutaConfiguracion);
+    leerConfig();
+}
+
+void leerConfig() {
+        TIPO_INTERFAZ = config_get_string_value(configuracion, "TIPO_INTERFAZ");
+        TIEMPO_UNIDAD_TRABAJO = config_get_int_value(configuracion, "TIEMPO_UNIDAD_TRABAJO");
+        IP_KERNEL = config_get_string_value(configuracion, "IP_KERNEL");
+        PUERTO_KERNEL = config_get_string_value(configuracion, "PUERTO_KERNEL");
+        IP_MEMORIA = config_get_string_value(configuracion, "IP_MEMORIA");
+        PUERTO_MEMORIA = config_get_string_value(configuracion, "PUERTO_MEMORIA");
+        PATH_BASE_DIALFS = config_get_string_value(configuracion, "PATH_BASE_DIALFS");
+        BLOCK_SIZE = config_get_int_value(configuracion, "BLOCK_SIZE");
+        BLOCK_COUNT = config_get_int_value(configuracion, "BLOCK_COUNT");
+}
+
+void inicializarConexiones(){
+    inicializarConexionKernel();
+    inicializarConexionMemoria();
+}
+
+void inicializarConexionKernel()
+{
+    fd_kernel = crear_conexion(IP_KERNEL, PUERTO_KERNEL, logger_error);
+    crearHilo(&hilo_kernel, (void*)enviarMsjKernel, NULL, "Kernel", logger_auxiliar, logger_error);
+}
+
+void inicializarConexionMemoria()
+{
+    fd_memoria = crear_conexion(IP_MEMORIA, PUERTO_MEMORIA, logger_error);
+    crearHilo(&hilo_memoria, (void*)enviarMsjMemoria, NULL, "Memoria", logger_auxiliar, logger_error);
+
+}
+
+void enviarMsjMemoria(){
+    log_info(logger_auxiliar, "ENTRE BIEN");
+    enviar_mensaje("Hola, soy I/O!", fd_memoria);
+}
+
+void enviarMsjKernel(){
+    enviar_mensaje("Hola, soy I/O!", fd_kernel);
+}
+
+void terminarPrograma() {
+    log_destroy(logger_obligatorio);
+    log_destroy(logger_auxiliar);
+    log_destroy(logger_error);
+    config_destroy(configuracion);
+    liberar_conexion(fd_memoria);
+    liberar_conexion(fd_kernel);
 }
---
+++ entradasalida/src/entradasalida.h
@@ -0,0 +1,67 @@
+#ifndef ENTRADASALIDA_H_
+#define ENTRADASALIDA_H_
+
+/*---------LIBRERIAS---------*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "../../utils/src/config/configs.h"
+#include "../../utils/src/sockets/sockets.h"
+#include "../../utils/src/hilos/hilos.h"
+#include <commons/config.h>
+#include <commons/string.h>
+
+/*---------DEFINES---------*/
+
+#define rutaConfiguracion "../entradasalida.config"
+
+/*---------DATOS DE LA CONFIGURACION---------*/
+
+char* TIPO_INTERFAZ;
+int TIEMPO_UNIDAD_TRABAJO;
+char*IP_KERNEL;
+char* PUERTO_KERNEL;
+char* IP_MEMORIA;
+char* PUERTO_MEMORIA;
+char* PATH_BASE_DIALFS;
+int BLOCK_SIZE;
+int BLOCK_COUNT;
+
+/*---------ESTRUCTURAS PARA INFORMACION---------*/
+
+// Estructuras para informacion
+t_log* logger_obligatorio;
+t_log* logger_auxiliar;
+t_log* logger_error;
+t_config* configuracion;
+
+/*---------FILE DESCRIPTORS CONEXIONES---------*/
+
+int fd_memoria;
+int fd_kernel;
+
+/*---------VARIABLES---------*/
+
+
+
+/*---------HILOS---------*/
+
+pthread_t hilo_memoria;
+pthread_t hilo_kernel;
+
+/*---------FUNCIONES---------*/
+// Inicializa las variables
+void inicializar();
+void inicializarLogs();
+void inicializarConexionKernel();
+void inicializarConfig();
+void inicializarConexiones();
+void inicializarConexionKernel();
+void inicializarConexionMemoria();
+void leerConfig();
+void enviarMsjMemoria();
+void enviarMsjKernel();
+
+// Liberaramos espacio de memoria
+void terminarPrograma();
+#endif
