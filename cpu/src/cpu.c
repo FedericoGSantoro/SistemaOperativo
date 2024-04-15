10865ba64184f3508497877cd9d653f4ef0dac5b
FedericoGSantoro <fesantoro@frba.utn.edu.ar>
2024-04-15 01:49:14 +0000

    Descomentar conexion memoria

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]) {
     leerConfig();
 
     iniciarServidoresCpu();
-    //iniciarConexionCpuMemoria();
+    iniciarConexionCpuMemoria();
     while(esperarClientes());
     
     terminarPrograma();
