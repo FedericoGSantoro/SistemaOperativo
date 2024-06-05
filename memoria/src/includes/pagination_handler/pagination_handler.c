bcadc9773894156346bbc1aa4d59626b5f247355
FacundoStancanelliUTN <fstancanelli@frba.utn.edu.ar>
2024-06-05 03:20:11 +0000

    creacion e inicializacion de estructuras para espacio de usuario y tabla de paginas por proceso

---
+++ memoria/src/includes/pagination_handler/pagination_handler.c
@@ -0,0 +1,16 @@
+#include "./pagination_handler.h"
+
+int obtener_cant_pags(int size_proceso) {
+     return ceil(size_proceso / memConfig.tamPagina); //dependiendo del tamaño del proceso se obtiene la cantidad de paginas con la division entera
+}
+
+void inicializar_tabla_paginas(int pid) {
+    
+    const int size_proceso = 0;
+    const int cant_paginas = 0;
+    t_list* tabla_paginas = list_create();
+    char* pid_convertido = int_to_string(pid);
+    dictionary_put(cache_tabla_por_proceso, pid_convertido, tabla_paginas);
+
+    log_info(loggerOblig, "PID: %d - Tamaño: %d", pid, cant_paginas);
+}
